50b32c02b899d262e898cc766e53275c79d1601a
BogdanSavvy <bogdansavin315@gmail.com>
2023-09-05 16:28:23 +0300

    Environment setup

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const DashboardPage = () => {
+  return (
+    <div className="p-2 text-3xl">
+      Home AI-Saas <Button variant="default">123</Button>
+    </div>
+  );
+};
+
+export default DashboardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+const LandingPage = () => {
+  return (
+    <div>
+      <p className="p-4 text-2xl font-bold text-red-500">Landing page</p>
+    </div>
+  );
+};
+
+export default LandingPage;
