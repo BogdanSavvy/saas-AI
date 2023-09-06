82efb44d9c25e9e10ba63f347d98e0b8f6c7fa55
BogdanSavvy <bogdansavin315@gmail.com>
2023-09-06 11:49:30 +0300

    Added a clerk-authorization

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
-
+import { UserButton } from "@clerk/nextjs";
 const DashboardPage = () => {
   return (
     <div className="p-2 text-3xl">
       Home AI-Saas <Button variant="default">123</Button>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,6 +1,15 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
 const LandingPage = () => {
   return (
     <div>
+      <Link href="/sign-in">
+        <Button>login</Button>
+      </Link>
+      <Link href="/sign-up">
+        <Button>register</Button>
+      </Link>
       <p className="p-4 text-2xl font-bold text-red-500">Landing page</p>
     </div>
   );
